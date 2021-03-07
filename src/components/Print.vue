29801c539ca7485fb582641afd6ae4b12d6a05c3
Madhura Shah <madhura.shah@wwindia.com>
2021-03-07 17:52:28 +0530

    initial commit

---
+++ src/components/Print.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  mounted() {
+    this.myPrint();
+  },
+  methods: {
+    myPrint() {
+      var page = window.open("chanakya.pdf");
+      page.print();
+    },
+  },
+};
+</script>
+<style lang="stylus" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Print from './components/Print'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
+const routes = [
+  { path: '/print', component: Print }
+]
 
+const router = new VueRouter({
+  routes
+})
 new Vue({
+  router,
+  mode:"history",
   render: h => h(App),
 }).$mount('#app')
+
+
